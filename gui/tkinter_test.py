b600f8f8fce804673a6112e85e9ea4da57557e8f
sprotg <spr@sde.dk>
2019-08-27 10:31:07 +0200

    Første grafiske brugerflade

--- gui/tkinter_test.py
+++ gui/tkinter_test.py
@@ -1,1 +1,25 @@
-print('hej')
+import tkinter as tk
+
+
+
+class Min_gui(tk.Frame):
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+
+        self.build_GUI()
+
+    def change(self):
+        self.lblName.config(text = 'Her står der noget nyt tekst.')
+
+
+    def build_GUI(self):
+        self.pack(side = tk.BOTTOM)
+        self.lblName = tk.Label(self, text = 'her står laaaaaangt noget tekst i en label')
+        self.butChange = tk.Button(self, text = 'Lav teksten om', command = self.change)
+        self.lblName.pack(side = tk.LEFT)
+        self.butChange.pack(side = tk.LEFT)
+
+app = Min_gui()
+
+app.mainloop()
