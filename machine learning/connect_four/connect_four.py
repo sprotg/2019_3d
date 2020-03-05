2d085663658e153b0138834d6748c8e070fb0e5a
sprotg <spr@sde.dk>
2020-03-05 07:54:21 +0100

    connect four

---
+++ machine learning/connect_four/connect_four.py
@@ -0,0 +1,77 @@
+import pygame
+from game import Connect_four_game
+
+# Setup pygame
+pygame.init()
+screen = pygame.display.set_mode((1000, 800))#, pygame.FULLSCREEN)
+myfont = pygame.font.SysFont("monospace", 12)
+clock = pygame.time.Clock()
+
+# Initialize game variables
+done = False
+game = Connect_four_game()
+x_off = 130
+y_off = 130
+size = 70
+
+
+# tile vars
+player_colors = [(100,100,100), (255,30,30), (255,255,30)]
+
+
+def draw_game():
+    pygame.draw.rect(screen, (0,0,0), pygame.Rect(0,0,1000,800))
+    screen.blit(myfont.render("state: {}".format(game.state), 0, (255,255,255)), (30,30))
+
+    if 0 < game.state <= 4:
+        #Draw the board
+        for x in range(len(game.grid[0])):
+            for y in range(len(game.grid)):
+                pygame.draw.rect(screen, player_colors[game.grid[x][y]], pygame.Rect(x_off + size * x * 1.1, y_off + size * y * 1.1, size, size))
+        pos = pygame.mouse.get_pos()
+        if x_off <= pos[0] <= x_off + 6*(size*1.1) and y_off > pos[1]:
+            x = int((pos[0] - x_off)/(size*1.1))
+            pygame.draw.rect(screen, player_colors[game.turn()], pygame.Rect(x_off + size * x * 1.1, y_off + size * (-1) * 1.1, size, size))
+    elif game.state == 0:
+        screen.blit(myfont.render("Click to start", 0, (255,255,255)), (470,380))
+
+
+
+
+
+#Main game loop
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if game.state == 0:
+                game.state = 1
+
+            elif game.state == 1:
+                if x_off <= pos[0] <= x_off + 6*(size*1.1) and y_off > pos[1]:
+                    x = int((pos[0] - x_off)/(size*1.1))
+                    game.place(x)
+                    if game.win():
+                        game.state = 5
+                    else:
+                        game.state = 3
+            elif game.state == 3:
+                if x_off <= pos[0] <= x_off + 6*(size*1.1) and y_off > pos[1]:
+                    x = int((pos[0] - x_off)/(size*1.1))
+                    game.place(x)
+                    if game.win():
+                        game.state = 5
+                    else:
+                        game.state = 1
+            elif game.state == 5:
+                game.reset()
+                game.state == 1
+
+
+    draw_game()
+
+    #pygame kommandoer til at vise grafikken og opdatere 60 gange i sekundet.
+    pygame.display.flip()
+    clock.tick(60)
---
+++ machine learning/connect_four/game.py
@@ -0,0 +1,37 @@
+class Connect_four_game:
+
+    def __init__(self):
+        self.reset()
+        #States:
+        # 0: start
+        # 1: player 1
+        # 2: check for win?
+        # 3: player 2
+        # 4: check for win?
+        # 5: game over
+        # 6: reset game
+
+    def reset(self):
+        self.grid = [[0 for i in range(6)] for i in range(6)]
+        self.state = 0
+
+
+    def place(self, col):
+        if self.state == 1 or self.state == 3:
+            if self.grid[col][0] == 0:
+                #Valid move
+                i = 5
+                while self.grid[col][i] != 0:
+                    i -= 1
+                self.grid[col][i] = self.turn()
+                self.state += 1
+
+    def win(self):
+        #Check for win
+        return false
+
+    def turn(self):
+        if self.state < 3:
+            return 1
+        else:
+            return 2
